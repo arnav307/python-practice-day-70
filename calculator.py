b9ad6db5dc96c98b3db846f8e1aa9492247d27d1
Arnavghimire <ghimirearnav738@gmail.com>
2025-05-29 23:10:59 +0545

    add it

---
+++ calculator.py
@@ -0,0 +1,77 @@
+def operators():
+    list_of_add=[]
+    list_of_sub=[]
+    list_of_mul=[]
+    list_of_div=[]
+    list_of_mod=[]
+    list_of_reminder=[]
+    return list_of_add, list_of_sub, list_of_mul, list_of_div, list_of_mod, list_of_reminder  # return the list 
+
+def add(num_1,num_2):
+    list_of_add,list_of_sub, list_of_mul, list_of_div, list_of_mod, list_of_reminder =operators() 
+    add=num_1+num_2
+    list_of_add.append(add)
+    return list_of_add
+ 
+def sub(num_1,num_2):
+    list_of_add,list_of_sub, list_of_mul, list_of_div, list_of_mod, list_of_reminder =operators() 
+    sub=num_1-num_2
+    list_of_sub.append(sub)
+    return list_of_sub
+
+def mul(num_1,num_2):
+    list_of_add,list_of_sub, list_of_mul, list_of_div, list_of_mod, list_of_reminder =operators() 
+
+    mul=num_1*num_2
+    list_of_mul.append(mul)
+    return list_of_mul
+
+def div(num_1,num_2):
+    list_of_add,list_of_sub, list_of_mul, list_of_div, list_of_mod, list_of_reminder =operators() 
+    div=num_1/num_2
+    list_of_div.append(div)
+    return list_of_div
+
+def mod(num_1,num_2):
+    list_of_add,list_of_sub, list_of_mul, list_of_div, list_of_mod, list_of_reminder =operators() 
+    mod=num_1%num_2
+    list_of_mod.append(mod)
+    return list_of_mod
+
+def reminder(num_1,num_2):
+    list_of_add,list_of_sub, list_of_mul, list_of_div, list_of_mod, list_of_reminder =operators() 
+    reminder=num_1%num_2
+    list_of_reminder.append(reminder)
+    return list_of_reminder
+    
+
+
+ 
+
+def main_play():
+    player_input=input("Enter which operation would you like to perform- add/sub/mul/div/mod/reminder :")
+    num_1=int(input("Enter a first number: "))
+    num_2=int(input("Enter a second number: "))
+    if player_input=='add':
+        print(add(num_1,num_2))
+    elif player_input=="sub":
+         print(sub(num_1,num_2))
+    elif player_input=="mul":
+         print(mul(num_1,num_2))
+    elif player_input=='div':
+        print(div(num_1,num_2))
+    elif player_input=='mod':
+        print(mod(num_1,num_2))
+    elif player_input=='reminder':
+        print(reminder(num_1,num_2))
+    else:
+        print("Invalid choice. Enter add/sub/mul/mod/div/reminder")
+    
+main_play()
+
+
+
+    
+
+
+    
